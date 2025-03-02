22a890150cf66ced031f02829f394879dd390d26
Chandrakanta Satapathy <114939097+techcs7646@users.noreply.github.com>
2025-03-02 22:52:18 +0530

    Update mailer.js

--- controller/mailer.js
+++ controller/mailer.js
@@ -1,34 +1,25 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-// Create a transporter object
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for other ports
+    host: process.env.EMAIL_HOST,  // Use Ethereal SMTP host
+    port: process.env.EMAIL_PORT,  // SMTP port (587 for TLS)
+    secure: false,                 // Set to false since Ethereal uses STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+        pass: process.env.EMAIL_PASS
+    }
 });
 
-// Function to send an email
-const sendEmail = async (to, subject, text) => {
-    try {
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to,
-            subject,
-            text,
-        };
+async function sendEmail(to, subject, text) {
+    const mailOptions = {
+        from: `"Test Sender" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        text
+    };
 
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response);
-        return info;
-    } catch (error) {
-        console.error('Error sending email:', error);
-        throw error;
-    }
-};
+    return transporter.sendMail(mailOptions);
+}
 
 module.exports = { sendEmail };
