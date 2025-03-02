5e607d741fcdbb27bf0205bf25a833cd345aa9e2
Chandrakanta Satapathy <114939097+techcs7646@users.noreply.github.com>
2025-03-02 22:51:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.post('/send-email', async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
